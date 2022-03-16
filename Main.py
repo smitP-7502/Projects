cf8cc3ca6c63c83643177e5c6921a249c0b40abf
smitP <smit@gmail.com>
2022-03-16 16:37:53 +0530

    Using playlist and search to play music is done

--- Main.py
+++ Main.py
@@ -1,7 +1,12 @@
 import time
+import Make_Playlist
+from Song_Loader import load_songs
+from Player import Music_Player
+import Search
 
 
 if __name__ == "__main__":
+    load_songs()
 
     while True:
         print("Enter the number")
@@ -9,12 +14,17 @@ if __name__ == "__main__":
         print("2. Search and Play")
         print("3. Play shuffle ")
         print("4. Exit from the Player")
-        n = input("Enter the number : ")
+        n = int(input("Enter the number : "))
 
         if n == 1:
             print("Make Playlist")
+            Make_Playlist.Show_song()
+            List = Make_Playlist.Playlist()
+            Music_Player(List)
         elif n == 2:
             print("Search and play")
+            List = Search.Search_Play()
+            Music_Player(List)
         elif n == 3:
             print("Play shuffle")
         elif n == 4:
